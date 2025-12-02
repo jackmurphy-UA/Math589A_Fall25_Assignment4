261328f00626d10d1c3e6acafdf1c8fd84426a74
jackmurphy-UA <jackmurphy669@gmail.com>
2025-12-02 16:54:44 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(void)
     double x1, x2;
     int iters;
 
-    int rc = fixed_point_solve(0.5, 0.5, 1e-6, 1000, &x1, &x2, &iters,scale=0.1);
+    int rc = fixed_point_solve(0.5, 0.5, 1e-6, 1000, &x1, &x2, &iters,scale);
 
     if (rc == 0) {
         printf("Converged:\n");
