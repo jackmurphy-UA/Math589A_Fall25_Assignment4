8f57a7d2286fed85f517e3adae953e01fb3f4721
jackmurphy-UA <jackmurphy669@gmail.com>
2025-12-02 16:56:35 -0700

    Update fp_wrapper.py

--- fp_wrapper.py
+++ fp_wrapper.py
@@ -22,7 +22,7 @@ def solve_system(x1_0, x2_0, tol, max_iter,scale=0.1):
     iters_out = ctypes.c_int()
 
     rc = _lib.fixed_point_solve(
-        x1_0, x2_0, tol, max_iter, scale
+        x1_0, x2_0, tol, max_iter,
         ctypes.byref(x1_out), ctypes.byref(x2_out), ctypes.byref(iters_out), scale
     )
     if rc != 0:
